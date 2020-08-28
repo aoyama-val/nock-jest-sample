3f3adcec2737dae666cdf9a30c9d537845f96282
aoyama <aoyama@val.co.jp>
2020-08-28 12:28:00 +0900

    add aws-sdk-mock.test.js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # nock + jestのサンプル
+
+- index.test.js
+    - nockのサンプル
+    - aws-sdkのDynamoDBもnockでモック化しようとしたが、なぜかうまく動かなかった
+- aws-sdk-mock.test.js
+    - aws-sdk-mockを使ってDynamoDBをモック化したサンプル
---
+++ aws-sdk-mock.test.js
@@ -0,0 +1,59 @@
+const nock = require("nock");
+const index = require("./index.js");
+const AWS = require("aws-sdk-mock");
+
+AWS.mock("DynamoDB.DocumentClient", "put", function(params, callback) {
+    callback(null, {});
+});
+
+AWS.mock("DynamoDB.DocumentClient", "query", function(params, callback) {
+    callback(null, 
+        {
+            Items: [
+                {
+                    category: 'mogemoge',
+                    key: 'asd',
+                    createdAt: new Date().toISOString(),
+                }
+            ],
+            Count: 1,
+            ScannedCount: 1
+        }
+    );
+});
+
+describe("index", () => {
+    beforeAll(() => {
+        nock.disableNetConnect();
+    });
+
+    afterAll(() => {
+        nock.enableNetConnect();
+    });
+
+    beforeEach(() => {
+        nock.cleanAll();
+    });
+
+    test("dynamoDbPut", async () => {
+        let res = await index.dynamoDbPut();
+        expect(res).toEqual({});
+    });
+
+    test("dynamoDbQuery", async () => {
+        let res = await index.dynamoDbQuery();
+        expect(res).toEqual(
+            {
+                Items: [
+                    {
+                        category: 'mogemoge',
+                        key: 'asd',
+                        createdAt: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/)
+                    }
+                ],
+                Count: 1,
+                ScannedCount: 1
+            }
+        );
+    });
+});
